a9913d453c9121ef48f27e25c8390884bb7b107f
Liam <lnguyen@ucsd.edu>
2022-10-28 21:44:15 -0700

    Done expose and explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,62 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById('voice-select')
+  let selectedLanguage
+
+  const textElement = document.getElementById('text-to-speak')
+  let textToSpeechValue = ''
+
+  const talkButton = document.querySelector('button')
+
+  const smileImage = document.querySelector("img[alt=\"Smiling face\"]")
+  const smileyFace = '/assets/images/smiling.png'
+  const smileyFaceOpen = '/assets/images/smiling-open.png'
+
+  let voices = []
+  synth.addEventListener('voiceschanged', () => {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement('option')
+      option.textContent = `${voices[i].name} (${voices[i].lang})`
+
+      if (voices[i].default) {
+        option.textContent += ' - DEFAULT'
+      }
+
+      option.setAttribute('data-lang', voices[i].lang)
+      option.setAttribute('data-name', voices[i].name)
+      voiceSelect.appendChild(option)
+    }
+  })
+
+  textElement.onchange = (e) => {
+    textToSpeechValue = textElement.value
+  }
+
+  voiceSelect.onchange = (e) => {
+    selectedLanguage = e.target.options[e.target.selectedIndex].getAttribute('data-name')
+  }
+
+  talkButton.onclick = (e) => {
+    e.preventDefault()
+
+    const utterThis = new SpeechSynthesisUtterance(textToSpeechValue)
+
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedLanguage) {
+        utterThis.voice = voices[i]
+      }
+    }
+
+    utterThis.onstart = () => {
+      smileImage.src = smileyFaceOpen
+    }
+    utterThis.onend = () => {
+      smileImage.src = smileyFace
+    }
+    synth.speak(utterThis)
+  }
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,40 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  let hornSelection = document.getElementById('horn-select');
+  let audioElement = document.querySelector('audio');
+  let playButton = document.querySelector('button');
+  let hornImage = document.querySelector("img[src=\"assets/images/no-image.png\"]")
+  let volumeSlider = document.getElementById('volume-controls').getElementsByTagName('input')[0]
+  let volumeImage = document.querySelector("img[src=\"assets/icons/volume-level-2.svg\"]")
+  let selectedOption;
+  const jsConfetti = new JSConfetti();
+
+  hornSelection.addEventListener('change', (e) => {
+    selectedOption = e.target.value;
+    audioElement.src = `/assets/audio/${selectedOption}.mp3`
+    hornImage.src = `/assets/images/${selectedOption}.svg`
+  })
+
+  playButton.onclick = (e) => {
+    audioElement.play()
+
+    if (selectedOption === 'party-horn')
+      jsConfetti.addConfetti()
+  }
+
+  volumeSlider.onchange = (e) => {
+    let level
+    if (volumeSlider.value == 0)
+      level = 0      
+    else if (volumeSlider.value < 33)
+      level = 1
+    else if (volumeSlider.value < 67)
+      level = 2
+    else
+      level = 3
+
+    volumeImage.src = `/assets/icons/volume-level-${level}.svg`
+    audioElement.volume = (volumeSlider.value / 100)
+  }
 }
